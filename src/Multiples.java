98911b58feabf3b0b8803423d607d3a0a01453f6
vdomash <viktor.domashnev@gmail.com>
2024-09-13 04:01:42 -0400

    markus tasks

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,7 @@
 public class Multiples {
     public static void main(String[] args) {
         int count = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 1000; i++) {
             if (divisible(i)) {
                 count++;
             }
